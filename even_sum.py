3608b5a5bff8f1fc8c4f4f1c3d33e7fd374ed0a0
charleszxresente-ai <charleszxresente@gmail.com>
2026-09-18 21:07:22 +0800

    Create even_sum.py

---
+++ even_sum.py
@@ -0,0 +1,5 @@
+# Challenge 2: The Array Sum
+def sum_even_numbers(numbers):
+    # TODO: Write a function that takes an array/list of integers and returns 
+    # the sum of all EVEN numbers only, ignoring odd numbers.
+    pass

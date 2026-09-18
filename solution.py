bcadf3db48d7747461736616612eeb4c064bdea9
charleszxresente-ai <charleszxresente@gmail.com>
2026-09-18 21:06:23 +0800

    Create solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+# Challenge 1: The Reverse Greeting Function
 def reverse_greeting(text):
-    # TODO: Write code that reverses 'text' and converts it to uppercase
+    # TODO: Write a function that takes a string and returns it reversed and in UPPERCASE.
+    # Example: "Hello World" -> "DLROW OLLEH"
     pass
